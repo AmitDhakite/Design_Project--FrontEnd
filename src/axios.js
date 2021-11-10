da156548e4250060713e9cb4bd95bc56c9ccf456
Amit Dhakite <amitdhakite24@gmail.com>
2021-11-10 08:46:40 +0530

    Redirects file

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: ,
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://design-project-iiecportal.herokuapp.com",
 });
 
 export default instance;
